98ddf75a1db92a489b3ab44571459d260bdaef37
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-11-14 12:02:09 -0800

    created and stored friends data array in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+//Data array of objects
+const friends = [
+  {
+    name: `Jon Thomas`,
+    photo: ``,
+    scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  },
+  {
+    name: `Jonny Rosero`,
+    photo: ``,
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: `Josh Hawthorne`,
+    photo: ``,
+    scores: [1, 3, 4, 5, 2, 4, 2, 1, 5, 5]
+  },
+  {
+    name: `Adam Luke`,
+    photo: ``,
+    scores: [2, 3, 4, 5, 4, 5, 3, 2, 2, 1]
+  },
+  {
+    name: `Denise Saldana`,
+    photo: ``,
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: `Christie Heil`,
+    photo: ``,
+    scores: [2, 3, 5, 5, 4, 1, 3, 1, 4, 5]
+  },
+  {
+    name: `John Doe`,
+    photo: ``,
+    scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+  },
+  {
+    name: `Jane Doe`,
+    photo: ``,
+    scores: [4, 3, 4, 3, 2, 4, 5, 4, 2, 1]
+  }
+]
+
+module.exports = friends
