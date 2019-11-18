ce6738435b6e34f1ef887ee31f751b17c1731346
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2019-11-17 18:18:03 -0800

    added the html code for home and survey pages.

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app)
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app)
 
 
 //Listen on a port
